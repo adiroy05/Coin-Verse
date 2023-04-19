c44c41f074069aeed4463e79397a326d315e65db
adiroy05 <96076675+adiroy05@users.noreply.github.com>
2023-04-19 17:33:05 +0530

    Add files via upload

---
+++ src/EXT/ExT.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import  Header  from '../Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
+import './EXT.css';
+
+function App1() {
+  return (
+    <GlobalProvider>
+     <Header state = {false} />
+    <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App1;
